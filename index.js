bea96e0eb2504656ea99330dad7f4ab1acefd153
Aditya425148 <aadi100right@gmail.com>
2025-07-06 23:44:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var score1=Math.floor(Math.random()*6)+1;
-var image1="./images/dice"+score1+".png";
+var image1="./dice"+score1+".png";
 document.querySelectorAll("img")[0].setAttribute("src",image1);
 var score2=Math.floor(Math.random()*6)+1;
-var image2="./images/dice"+score2+".png";
+var image2="./dice"+score2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",image2);
 if(image1>image2){
     document.querySelector("h1").innerHTML="🚩Player 1 Wins!"
